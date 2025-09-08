ef60a95ca3ca843f7a6fdb5f691dbc7eff69ff8f
0x26d0 <190246422+26d0@users.noreply.github.com>
2025-09-08 16:50:16 +0900

    動的レンダリングを強制的に静的に設定するために、Open GraphおよびTwitterカードの画像コンポーネントにdynamicプロパティを追加

--- src/app/twitter-image.tsx
+++ src/app/twitter-image.tsx
@@ -1,6 +1,7 @@
 import { ImageResponse } from 'next/og';
 
 export const runtime = 'edge';
+export const dynamic = 'force-static';
 
 export const alt = 'ccQuiz - Country Code Quiz';
 export const size = {
